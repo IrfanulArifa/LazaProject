f9b493ac2d90b7ab67f737b0ed9a51f02832566d
Irfanul Arifa <arifa.aja16@gmail.com>
2023-08-03 14:26:34 +0700

    Fixing Detail Page Constraint and Data

---
+++ LazaProject/Controller/AfterLogin/Detail/TableViewCell/Cell/SizeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SizeCollectionViewCell.swift
+//  LazaProject
+//
+//  Created by Irfanul Arifa on 03/08/23.
+//
+
+import UIKit
+
+class SizeCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var sizeLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+}
--- LazaProject/Controller/AfterLogin/Detail/TableViewCell/DetailTableViewCell.swift
+++ LazaProject/Controller/AfterLogin/Detail/TableViewCell/DetailTableViewCell.swift
@@ -21,12 +21,44 @@ class DetailTableViewCell: UITableViewCell {
   @IBOutlet weak var star3: UIImageView!
   @IBOutlet weak var star4: UIImageView!
   @IBOutlet weak var star5: UIImageView!
+  
+  let dummy = ["S", "M", "L", "XL", "XXL", "XXXL", "XXXXL"]
+  
+  @IBOutlet weak var sizeCollection: UICollectionView!
   override func awakeFromNib() {
     super.awakeFromNib()
-    // Initialization code
+    
+    sizeCollection.dataSource = self
+    sizeCollection.delegate = self
+    sizeCollection.register(UINib(nibName: "SizeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "SizeCollectionViewCell")
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
+  }
+}
+
+extension DetailTableViewCell: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return dummy.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let data = dummy[indexPath.item]
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SizeCollectionViewCell", for: indexPath) as? SizeCollectionViewCell else { return UICollectionViewCell() }
+    cell.sizeLabel.text = data
+    return cell
+  }
+  
+  
+}
+
+extension DetailTableViewCell: UICollectionViewDelegate {
+  
+}
+
+extension DetailTableViewCell: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 60, height: 60)
   }
 }
